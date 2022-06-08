eea3549814076fa6263577a144e2e6e1bd66ccf2
Thomas Denecker <thomas.denecker@gmail.com>
2022-06-08 09:59:53 +0200

    Compile data

---
+++ data/README.md
@@ -0,0 +1,18 @@
+# Les données
+
+## Contenu du dossier
+
+Le dossier contient :
+- Un dossier de données brutes au format csv (voir source ci-dessous)
+    - 
+- Un code R pour générer un fichier unique TSV et en JSON ;
+- Un fichier de données en TSV pour une utilisation en R (ou autre) ;
+- Un fichier de données en JSON pour une utilisation en JS (ou autre). 
+
+## Source des données 
+
+FREE DATA FROM WORLD BANK VIA GAPMINDER.ORG, CC-BY LICENSE
+
+Liens vers les données :
+- les régions : https://www.gapminder.org/fw/four-regions/
+- Les autres données : https://www.gapminder.org/data/
---
+++ data/dataPreparation.R
@@ -0,0 +1,104 @@
+################################################################################
+# Title   : Génération d'un fichier TSV et JSON à partir de données de gapminder
+# Source  : Gapminder (https://www.gapminder.org/data/)
+# Dev     : Thomas DENECKER
+# Date    : 2022-06-08
+# GitHub  : https://github.com/IFB-ElixirFr/comparison_JS_graphics_libraries
+################################################################################
+
+################################################################################
+###                                Library                                   ###
+################################################################################
+
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(jsonlite)
+
+################################################################################
+###                              FUNCTION                                    ###
+################################################################################
+
+
+convert_units <- function(x) {
+  
+  if (class(x) == "numeric") return(x)
+
+  # named vector of scalings (you can add to this)
+  unit_scale <- c("k" = 1e3, "m" = 1e6, "b" = 1e9)
+  
+  # clean up some potential nuisances with the input
+  x_str <- gsub(",", "", trimws(tolower(as.character(x))))
+  
+  # extract out the letters
+  unit_char <- gsub("[^a-z]", "", x_str)
+  
+  # extract out the numbers and convert to numeric
+  x_num <- as.numeric(gsub("[a-z]", "", x_str), "", x_str)
+  
+  # develop a vector of multipliers
+  multiplier <- unit_scale[match(unit_char, names(unit_scale))]
+  multiplier[is.na(multiplier)] <- 1
+  
+  # multiply
+  x_num * multiplier
+}
+
+################################################################################
+###                                 MAIN                                     ###
+################################################################################
+
+#-------------------------------------------------------------------------------
+# Lecture des données
+#-------------------------------------------------------------------------------
+
+income <- read.csv2("raw/income_per_person.csv", sep = ",")
+live_expectancy <- read.csv2("raw/life_expectancy_years.csv", sep = ",")
+pop_total <- read.csv2("raw/population_total.csv", sep = ",")
+regions <- read.csv2("raw/list-of-countries-etc.tsv", sep = "\t")
+
+#-------------------------------------------------------------------------------
+# Extract by year (2021)
+#-------------------------------------------------------------------------------
+
+# Income
+income_2021 <- income  %>% 
+  select(country, X2021) %>% 
+  rename(income_2021 = X2021) %>% 
+  mutate(income_2021 =  convert_units(income_2021))
+
+# live_expectancy
+live_expectancy_2021 <- live_expectancy  %>% 
+  select(country, X2021) %>% 
+  rename(live_expectancy_2021 = X2021) %>% 
+  mutate(live_expectancy_2021 =  as.numeric(live_expectancy_2021))
+
+# pop_total
+pop_total_2021 <- pop_total  %>% 
+  select(country, X2021) %>% 
+  rename(pop_total_2021 = X2021) %>% 
+  mutate(pop_total_2021 =  convert_units(pop_total_2021))
+
+# region
+regions_area <- regions %>% 
+  select(name, six_regions) %>% 
+  rename(country = name)
+
+#-------------------------------------------------------------------------------
+#  Merge
+#-------------------------------------------------------------------------------
+
+final_dataset <- regions_area %>% full_join(pop_total_2021, by='country') %>% 
+  full_join(income_2021, by='country') %>% 
+  full_join(live_expectancy_2021, by='country') %>% drop_na()
+
+#-------------------------------------------------------------------------------
+#  Export
+#-------------------------------------------------------------------------------
+
+# TSV
+write_tsv(final_dataset, "compileDataGapminder.tsv")
+
+# Json
+write(toJSON(final_dataset), "compileDataGapminder.json" )
+
